91b3283e0e6cadedc5c2a11e3782fb21013df812
Matt Hayes <matt@mysterycommand.com>
2018-10-27 12:55:00 -0400

    feat(lib/rtc-data.ts): more fleshing out the constructor/class/options

--- src/lib/rtc-data.ts
+++ src/lib/rtc-data.ts
@@ -1,4 +1,4 @@
-import { Duplex } from 'readable-stream';
+import { Duplex, DuplexOptions } from 'readable-stream';
 
 export const MAX_BUFFERED_AMOUNT = 64 * 1024;
 export const ICE_COMPLETE_TIMEOUT = 5 * 1000;
@@ -9,12 +9,36 @@ function randomBytes(length = 0): Buffer {
   return Buffer.from(bytes.buffer);
 }
 
+interface RtcDataOptions extends DuplexOptions {
+  initiator?: boolean;
+  channelName?: string;
+}
+
 export default class RtcData extends Duplex {
-  constructor(
-    public readonly id = randomBytes(4)
-      .toString('hex')
-      .slice(0, 7),
-  ) {
-    super();
+  public readonly id = randomBytes(4)
+    .toString('hex')
+    .slice(0, 7);
+
+  private initiator: boolean;
+  private channelName: string | null;
+  private connection: RTCPeerConnection;
+
+  constructor(opts: RtcDataOptions) {
+    super({ ...opts, allowHalfOpen: false });
+
+    this.initiator = opts.initiator || false;
+    this.channelName = this.initiator
+      ? opts.channelName || randomBytes(20).toString('hex')
+      : null;
+
+    this.connection = new RTCPeerConnection();
+    if (this.initiator) {
+      // setup data
+      this.connection.createDataChannel(this.channelName as string, {});
+    } else {
+      this.connection.addEventListener('datachannel', event => {
+        console.log(event); // tslint:disable-line
+      });
+    }
   }
 }
