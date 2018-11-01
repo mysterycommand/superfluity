0bc6b3f0d897ed3f205ca38740ab7cc7a16033f4
Matt Hayes <matt@mysterycommand.com>
2018-10-31 23:54:32 -0400

    refactor(main.ts): a simple host-only, anonymously signed in experience

--- src/main.ts
+++ src/main.ts
@@ -1,85 +1,111 @@
+import { User } from 'firebase';
+import Peer from 'simple-peer';
+
+import { auth, database } from './lib/firebase';
+
 import './main.css';
 
-import Peer from 'simple-peer';
-import database from './lib/database';
+const pre = document.querySelector('pre') as HTMLPreElement;
 
-database.ref('/offer').on('value', snapshot => {
-  if (!snapshot || !snapshot.val()) {
+auth.signInAnonymously().then(snapshot => {
+  if (!snapshot) {
     return;
   }
 
-  console.log('offer', snapshot.val());
+  const user = snapshot.user as User;
+  const host = new Peer({ initiator: true, trickle: false });
+  Object.defineProperty(window, 'host', { value: host });
 
-  if (!isInitiator()) {
-    peer.signal(JSON.stringify(snapshot.val()));
-  }
-});
-
-database.ref('/answer').on('value', snapshot => {
-  if (!snapshot || !snapshot.val()) {
-    return;
-  }
-
-  if (isInitiator()) {
-    peer.signal(JSON.stringify(snapshot.val()));
-  }
-});
-
-const isInitiator = () => location.hash === '#1';
-
-const peer = new Peer({ initiator: isInitiator(), trickle: false });
-Object.defineProperty(window, 'peer', {
-  value: peer,
-});
-
-peer.on('error', (error: Error) => {
-  console.log('error', error); // tslint:disable-line
-});
-
-peer.on('signal', (data: {}) => {
-  console.log('SIGNAL', JSON.stringify(data)); // tslint:disable-line
-  (document.querySelector(
-    '#outgoing',
-  ) as HTMLPreElement).textContent = JSON.stringify(data);
-
-  if (isInitiator()) {
-    database.ref('/offer').set(data);
-  } else {
-    database.ref('/answer').set(data);
-  }
-});
-
-(document.querySelector('form') as HTMLFormElement).addEventListener(
-  'submit',
-  (event: Event) => {
-    event.preventDefault();
-    peer.signal(
-      JSON.parse(
-        (document.querySelector('#incoming') as HTMLTextAreaElement).value,
-      ),
+  host.on('signal', data => {
+    pre.innerText = JSON.stringify(
+      {
+        ...data,
+        sdp: data.sdp.split('\r\n'),
+      },
+      null,
+      2,
     );
-  },
-);
-
-peer.on('connect', () => {
-  console.log('CONNECT'); // tslint:disable-line
-  peer.send(JSON.stringify({ whatever: Math.random() }));
+    database.ref(`/hosts/${user.uid}`).set(data);
+  });
 });
 
-peer.on('data', (data: string) => {
-  console.log('data:', JSON.parse(data)); // tslint:disable-line
-});
+// database.ref('/offer').on('value', snapshot => {
+//   if (!snapshot || !snapshot.val()) {
+//     return;
+//   }
 
-window.addEventListener('deviceorientation', event => {
-  event.preventDefault();
-  const { absolute, alpha, beta, gamma } = event;
-  peer.send(JSON.stringify({ absolute, alpha, beta, gamma }));
-});
+//   console.log('offer', snapshot.val());
 
-window.addEventListener('load', () => {
-  if (isInitiator()) {
-    database.ref('/offer').set(null);
-  }
+//   if (!isInitiator()) {
+//     peer.signal(JSON.stringify(snapshot.val()));
+//   }
+// });
 
-  database.ref('/answer').set(null);
-});
+// database.ref('/answer').on('value', snapshot => {
+//   if (!snapshot || !snapshot.val()) {
+//     return;
+//   }
+
+//   if (isInitiator()) {
+//     peer.signal(JSON.stringify(snapshot.val()));
+//   }
+// });
+
+// const isInitiator = () => location.hash === '#1';
+
+// const peer = new Peer({ initiator: isInitiator(), trickle: false });
+// Object.defineProperty(window, 'peer', {
+//   value: peer,
+// });
+
+// peer.on('error', (error: Error) => {
+//   console.log('error', error); // tslint:disable-line
+// });
+
+// peer.on('signal', (data: {}) => {
+//   console.log('SIGNAL', JSON.stringify(data)); // tslint:disable-line
+//   (document.querySelector(
+//     '#outgoing',
+//   ) as HTMLPreElement).textContent = JSON.stringify(data);
+
+//   if (isInitiator()) {
+//     database.ref('/offer').set(data);
+//   } else {
+//     database.ref('/answer').set(data);
+//   }
+// });
+
+// (document.querySelector('form') as HTMLFormElement).addEventListener(
+//   'submit',
+//   (event: Event) => {
+//     event.preventDefault();
+//     peer.signal(
+//       JSON.parse(
+//         (document.querySelector('#incoming') as HTMLTextAreaElement).value,
+//       ),
+//     );
+//   },
+// );
+
+// peer.on('connect', () => {
+//   console.log('CONNECT'); // tslint:disable-line
+//   peer.send(JSON.stringify({ whatever: Math.random() }));
+// });
+
+// peer.on('data', (data: string) => {
+//   console.log('data:', JSON.parse(data)); // tslint:disable-line
+// });
+
+// window.addEventListener('deviceorientation', event => {
+//   event.preventDefault();
+//   const { absolute, alpha, beta, gamma } = event;
+//   peer.send(JSON.stringify({ absolute, alpha, beta, gamma }));
+// });
+
+// window.addEventListener('load', () => {
+//   if (isInitiator()) {
+//     database.ref('/offer').set(null);
+//   }
+
+//   database.ref('/answer').set(null);
+// });
