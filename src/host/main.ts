9661152e02fd81dfa1ac9e2ac9704d37ddf431a0
Matt Hayes <matt@mysterycommand.com>
2018-11-09 12:26:37 -0500

    feat(host): add a nicer looking auto updating logger to the works

--- src/host/main.ts
+++ src/host/main.ts
@@ -7,6 +7,14 @@ import './main.css';
 const pre = document.querySelector('pre') as HTMLPreElement;
 const div = document.querySelector('div') as HTMLDivElement;
 
+const log = (str: string) => {
+  pre.textContent += str;
+  pre.scrollTo({
+    top: pre.scrollHeight,
+    behavior: 'smooth',
+  });
+};
+
 auth.signInAnonymously().then(userCredential => {
   if (!(userCredential && userCredential.user)) {
     return;
@@ -34,9 +42,9 @@ auth.signInAnonymously().then(userCredential => {
       }
 
       if (error) {
-        pre.textContent += `error: ${error.message}\n\n`;
+        log(`error: ${error.message}\n\n`);
       } else if (guests[connection.key]) {
-        pre.textContent += `player ${connection.key} - disconnected\n\n`;
+        log(`player ${connection.key} - disconnected\n\n`);
       }
 
       if (guests[connection.key]) {
@@ -54,7 +62,7 @@ auth.signInAnonymously().then(userCredential => {
         answer.set({ ...data, uid });
       })
       .on('connect', () => {
-        pre.textContent += `player ${connection.key} - connected\n\n`;
+        log(`player ${connection.key} - connected\n\n`);
 
         const time = new Date().toLocaleTimeString();
         host.send(JSON.stringify({ host: connection.key, time }));
@@ -68,7 +76,7 @@ auth.signInAnonymously().then(userCredential => {
         if (parsed.time) {
           const message = JSON.stringify(parsed, null, 2);
           const time = new Date().toLocaleTimeString();
-          pre.textContent += `/* ${time} */\n${message}\n\n`;
+          log(`/* ${time} */\n${message}\n\n`);
         } else {
           div.style.transform = [
             `rotateY(${parsed.alpha - 180}deg)`,
@@ -92,7 +100,7 @@ auth.signInAnonymously().then(userCredential => {
       return;
     }
 
-    pre.textContent += `player ${connection.key} - removed\n\n`;
+    log(`player ${connection.key} - removed\n\n`);
 
     if (guests[connection.key]) {
       if (guests[connection.key] instanceof Peer) {
