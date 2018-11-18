000e44806c980331fcc02de49c85e196b0967f90
Matt Hayes <matt@mysterycommand.com>
2018-11-18 00:22:22 -0500

    refactor(host): guests is now a hash of Guest objects, and orientation is only updated in rAF based

--- src/host/main.ts
+++ src/host/main.ts
@@ -1,8 +1,8 @@
-import Peer, { Instance } from 'simple-peer';
+import Peer from 'simple-peer';
 import { toCanvas, QRCodeRenderersOptions } from 'qrcode';
 
 import { auth, database } from '../lib/firebase';
-import { DataSnapshot, SignalData } from '../lib/types';
+import { DataSnapshot, SignalData, Guest } from '../lib/types';
 
 import './main.css';
 
@@ -31,18 +31,56 @@ toCanvas(canvas, playerUrl, qrcodeOpts, error => {
   }
 });
 
+const guests: Record<string, Guest> = {};
+if (process.env.NODE_ENV === 'development') {
+  Object.defineProperty(window, 'guests', { value: guests });
+}
+
+let pt = performance.now();
+let dt = 0;
+
+const draw = (t: DOMHighResTimeStamp) => {
+  requestAnimationFrame(draw);
+
+  dt = t - pt;
+  pt = t;
+
+  Object.entries(guests).forEach(([key, { orientation, acceleration }]) => {
+    orientation.alpha += acceleration.alpha * (dt / 1000);
+    orientation.beta += acceleration.beta * (dt / 1000);
+    orientation.gamma += acceleration.gamma * (dt / 1000);
+
+    const li = document.getElementById(`connection-${key}`);
+    if (!li) {
+      return;
+    }
+
+    const xf = [
+      `rotateX(${Math.round(orientation.alpha)}deg)`,
+      `rotateY(${Math.round(orientation.beta)}deg)`,
+      `rotateZ(${Math.round(-orientation.gamma)}deg)`,
+    ];
+
+    li.style.transform = xf.join(' ');
+    if (process.env.NODE_ENV === 'development') {
+      li.innerText = xf.join('\n');
+    }
+  });
+};
+
+requestAnimationFrame(draw);
+
 auth.signInAnonymously().then(userCredential => {
   if (!(userCredential && userCredential.user)) {
     return;
   }
 
   const { uid } = userCredential.user;
-  const guests: Record<string, Instance> = {};
-  Object.defineProperty(window, 'guests', { value: guests });
+  const connections = database.ref('/connections');
 
   const removeGuest = (key: string) => {
     if (guests[key]) {
-      const host = guests[key];
+      const { host } = guests[key];
       host.destroy();
 
       delete guests[key];
@@ -54,8 +92,6 @@ auth.signInAnonymously().then(userCredential => {
     }
   };
 
-  const connections = database.ref('/connections');
-
   const onConnectionAdded = (connection: DataSnapshot | null) => {
     if (!(connection && connection.key)) {
       return;
@@ -65,12 +101,20 @@ auth.signInAnonymously().then(userCredential => {
     const answer = connection.child('answer').ref;
 
     const host = new Peer({ initiator: false, trickle: false });
-    guests[connection.key] = host;
+    guests[connection.key] = {
+      acceleration: { alpha: 0, beta: 0, gamma: 0 },
+      orientation: { alpha: 0, beta: 0, gamma: 0 },
+      host,
+    };
 
     const li = document.createElement('li');
     li.id = `connection-${connection.key}`;
 
     const onSignal = (data: SignalData) => {
+      if (!(connection && connection.key)) {
+        return;
+      }
+
       if (data.type !== 'answer') {
         return;
       }
@@ -79,6 +123,10 @@ auth.signInAnonymously().then(userCredential => {
     };
 
     const onConnect = () => {
+      if (!(connection && connection.key)) {
+        return;
+      }
+
       log(`connection: ${connection.key} - connected\n`);
 
       ul.appendChild(li);
@@ -102,16 +150,13 @@ auth.signInAnonymously().then(userCredential => {
     };
 
     const onData = (data: string) => {
-      const {
-        time,
-        player,
-        width,
-        height,
-        absolute,
-        alpha,
-        beta,
-        gamma,
-      } = JSON.parse(data);
+      if (!(connection && connection.key)) {
+        return;
+      }
+
+      const { time, player, width, height, alpha, beta, gamma } = JSON.parse(
+        data,
+      );
 
       if (time) {
         const localTime = new Date().toLocaleTimeString();
@@ -137,18 +182,7 @@ auth.signInAnonymously().then(userCredential => {
         return;
       }
 
-      const xf = [
-        `rotateY(${alpha.toFixed(2)}deg)`,
-        `rotateX(${(beta + 180).toFixed(2)}deg)`,
-        `rotateZ(${(gamma * 2 + 180).toFixed(2)}deg)`,
-      ];
-
-      // TODO: @mysterycommand - more here, lerp toward these values for smoother look
-      li.style.transform = xf.join(' ');
-
-      if (process.env.NODE_ENV === 'development') {
-        li.innerText = `absolute: ${absolute}\n${xf.join('\n')}`;
-      }
+      guests[connection.key].acceleration = { alpha, beta, gamma };
     };
 
     const onOffer = (data: DataSnapshot | null) => {
