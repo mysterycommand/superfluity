6c39ebde0054e349b83a2525e55cdfbf4f2a957b
Matt Hayes <matt@mysterycommand.com>
2018-11-18 00:21:20 -0500

    feat(types): add a Guest type that's a player's host instance plus the player's orientation and acce

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,15 @@
+import { Instance } from 'simple-peer';
+
 export type DataSnapshot = firebase.database.DataSnapshot;
 
 export interface SignalData {
   type: 'offer' | 'answer';
   sdp: string;
   uid: string;
+}
+
+export interface Guest {
+  acceleration: { alpha: number; beta: number; gamma: number };
+  orientation: { alpha: number; beta: number; gamma: number };
+  host: Instance;
 }
